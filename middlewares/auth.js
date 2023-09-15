c92ea0ea75c6d141ffb838661907d07b0e390544
Vikosik99 <viktoria.b99@yandex.ru>
2023-09-15 18:43:36 +0300

    test8

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,8 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnautorizedError('Необходима авторизация');
+    next(new UnautorizedError('Необходима авторизация'));
+    return;
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -18,7 +19,8 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    throw new UnautorizedError('Необходима авторизация');
+    next(new UnautorizedError('Необходима авторизация'));
+    return;
   }
 
   req.user = payload;
